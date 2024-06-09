329b15f80b800b0e1e952a585f55afca117f0401
vikrant005 <87076440+vikrant005@users.noreply.github.com>
2024-06-09 17:52:28 +0530

    Add Appsetting.json in infrastructure

---
+++ EcommerceApp.Infrastructure/Data/VikrantDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceApp.Infrastructure.Data
+{
+    public class VikrantDbContextFactory : DesignTimeDbContextFactoryBase<VikrantDbContext>
+    {
+        protected override VikrantDbContext CreateNewInstance(DbContextOptions<VikrantDbContext> options)
+        {
+            return new VikrantDbContext(options);
+        }
+    }
+}
---
+++ EcommerceApp.Infrastructure/Migrations/20240609122153_InititalCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InititalCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    PhoneWithCountryCode = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    CountryCode = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CountryID = table.Column<long>(type: "bigint", nullable: false),
+                    CityID = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
